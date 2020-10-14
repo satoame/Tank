6a22810a4404ab53007e29a22cb18f1bdf4d4b53
sato daichi <ds44.roy415ksk@icloud.com>
2020-10-14 15:45:03 +0900

    追加

---
+++ Project/Assets/Script/CameraCtr.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraCtr : MonoBehaviour
+
+{   //メインカメラ
+    public GameObject mainCamera;
+    //サブカメラ
+    public GameObject subCamera;
+
+    private bool mainCameraIN = true;
+
+    void Start()
+    {
+        subCamera.SetActive(false);
+    }
+
+    //メインカメラ
+    public void MainCamera()
+    {
+        mainCamera.SetActive(true);
+        subCamera.SetActive(false);
+
+        mainCameraIN = true;
+    }
+
+    //サブカメラ
+    public void SubCamera()
+    {
+        mainCamera.SetActive(false);
+        subCamera.SetActive(true);
+
+        mainCameraIN = false;
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.F) && mainCameraIN == true)
+        {
+            SubCamera();
+        }
+        else if(Input.GetKeyDown(KeyCode.F) && mainCameraIN == false)
+        {
+            MainCamera();
+        }
+    }
+    
+}
