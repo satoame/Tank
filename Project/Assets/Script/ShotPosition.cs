6c615ce9888a578bc25fd2eda188ec9c6ba32ff6
satoame <ds44.roy415ksk@icloud.com>
2020-10-19 13:52:10 +0900

    追加

---
+++ Project/Assets/Script/ShotPosition.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShotPosition : MonoBehaviour
+{
+    public GameObject ShotPrefab;
+    public float shotSpeed;
+
+    void Start()
+    {
+        //生成
+        //GameObject shot = Instantiate(ShotPrefab, transform.position, Quaternion.identity);
+       // Rigidbody shotbr = shot.GetComponent<Rigidbody>();
+    }
+    void Update()
+    {
+        //ショット
+        if (Input.GetMouseButtonDown(0))
+        {
+            //生成
+            GameObject shot = Instantiate(ShotPrefab, transform.position, Quaternion.identity);
+            Rigidbody shotbr = shot.GetComponent<Rigidbody>();
+            //速度
+            shotbr.velocity = this.transform.forward * shotSpeed;
+
+            shot.transform.forward = this.transform.forward;
+        }
+    }
+}
