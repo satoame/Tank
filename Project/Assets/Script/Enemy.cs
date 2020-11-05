735791200021d1ad85aaed27ccee282be06018df
sato daichi <ds44.roy415ksk@icloud.com>
2020-11-05 18:39:31 +0900

    追加

---
+++ Project/Assets/Script/Enemy.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+	private CharacterController enemyController;
+	private Animator animator;
+	//　目的地
+	private Vector3 destination;
+	//　歩くスピード
+	[SerializeField]
+	private float walkSpeed = 1.0f;
+	//　速度
+	private Vector3 velocity;
+	//　移動方向
+	private Vector3 direction;
+	//　到着フラグ
+	private bool arrived;
+	//　スタート位置
+	private Vector3 startPosition;
+
+	void Start()
+	{
+		enemyController = GetComponent<CharacterController>();
+		animator = GetComponent<Animator>();
+		var randDestination = Random.insideUnitCircle * 2;
+		destination = startPosition + new Vector3(randDestination.x, 0, randDestination.y);
+		velocity = Vector3.zero;
+		arrived = false;
+		startPosition = transform.position;
+	}
+
+	void Update()
+	{
+		if (!arrived)
+		{
+			if (enemyController.isGrounded)
+			{
+				velocity = Vector3.zero;
+				animator.SetFloat("Speed", 2.0f);
+				direction = (destination - transform.position).normalized;
+				transform.LookAt(new Vector3(destination.x, transform.position.y, destination.z));
+				velocity = direction * walkSpeed;
+				Debug.Log(destination);
+			}
+			velocity.y += Physics.gravity.y * Time.deltaTime;
+			enemyController.Move(velocity * Time.deltaTime);
+
+			//　目的地に到着したかどうかの判定
+			if (Vector3.Distance(transform.position, destination) < 0.5f)
+			{
+				arrived = true;
+				animator.SetFloat("Speed", 0.0f);
+			}
+		}
+	}
+}
